00b86aaeef7ebd066046c95ec7df84af93ec9da5
Mykyta Rotskyi <mykyta.rotskyi@aeriagames.com>
2020-04-08 23:19:24 +0200

    Addde NOW instead of two datetimes

--- spider.py
+++ spider.py
@@ -6,10 +6,12 @@ from shutil import copyfile
 sys.path.append('/home/a4iv1kv14b88/covid_map/modules')
 from modules import requests
 
+NOW = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
 
 def generate_csv(datarows):
     """Helper to generate CSV from JSON"""
-    with open('/home/a4iv1kv14b88/covid_map/outputs/output_{}.csv'.format(datetime.now().strftime("%Y-%m-%dT%H:%M:%S")), 'w') as csvfile:
+    with open('/home/a4iv1kv14b88/covid_map/outputs/output_{}.csv'.format(NOW), 'w') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=['Active',
                                                      'Admin2',
                                                      'Combined_Key',
@@ -27,7 +29,7 @@ def generate_csv(datarows):
                                                      'Recovered'])
         writer.writeheader()
         writer.writerows(datarows)
-    copyfile("/home/a4iv1kv14b88/covid_map/outputs/output_{}.csv".format(datetime.now().strftime("%Y-%m-%dT%H:%M:%S")),
+    copyfile("/home/a4iv1kv14b88/covid_map/outputs/output_{}.csv".format(NOW),
              "/home/a4iv1kv14b88/genasys.com/public_html/wp-content/uploads/map.csv")
 
 
--- venues.py
+++ venues.py
@@ -6,14 +6,16 @@ from shutil import copyfile
 sys.path.append('/home/a4iv1kv14b88/covid_map/modules')
 from modules import requests
 
+NOW = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
 response = requests.get('https://api.findcovidtesting.com/api/v1/location')
 if not response.status_code == 200:
     exit(1)
 fields = max([list(i.keys()) for i in response.json()])
-with open('/home/a4iv1kv14b88/covid_map/outputs/venues_{}.csv'.format(datetime.now().strftime("%Y-%m-%dT%H:%M:%S")),
+with open('/home/a4iv1kv14b88/covid_map/outputs/venues_{}.csv'.format(NOW),
           'w') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=fields)
     writer.writeheader()
     writer.writerows(response.json())
-copyfile("/home/a4iv1kv14b88/covid_map/outputs/venues_{}.csv".format(datetime.now().strftime("%Y-%m-%dT%H:%M:%S")),
+copyfile("/home/a4iv1kv14b88/covid_map/outputs/venues_{}.csv".format(NOW),
              "/home/a4iv1kv14b88/genasys.com/public_html/wp-content/uploads/venues.csv")
